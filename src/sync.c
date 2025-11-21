17e91c039c6f3630cea5f6cf39a94372766333ef
Joakim <joakim.snokvist@gmail.com>
2025-11-21 18:40:13 +0100

    Add master sync logging for slot commands

--- src/sync.c
+++ src/sync.c
@@ -1260,10 +1260,12 @@ static int h_sync_register(struct mg_connection *c, void *ud) {
     }
 
     int previous_slot = rec->slot_index;
+    int previous_ack_generation = rec->last_ack_generation;
     assigned_slot = sync_master_auto_assign_slot_locked(&app->master, rec, &cfg);
     if (assigned_slot >= 0) {
         slot_generation = app->master.slot_generation[assigned_slot];
         int slot_changed = (previous_slot != assigned_slot);
+        const char *send_reason = "pending_generation";
         /*
          * Slot commands carry a generation value so slaves can avoid
          * re-running commands they have already applied. A slave reports the
@@ -1275,15 +1277,40 @@ static int h_sync_register(struct mg_connection *c, void *ud) {
          */
         if (slot_changed) {
             rec->last_ack_generation = 0;
+            send_reason = "slot_change";
         } else if (ack_generation > 0) {
             if (ack_generation > slot_generation) {
                 rec->last_ack_generation = 0;
+                send_reason = "ack_out_of_range";
             } else if (ack_generation > rec->last_ack_generation) {
                 rec->last_ack_generation = ack_generation;
+                send_reason = "ack_behind";
             }
         }
         if (slot_generation > rec->last_ack_generation) {
             send_generation = slot_generation;
+            if (rec->last_ack_generation <= 0 && !slot_changed) {
+                send_reason = "no_ack";
+            }
+        }
+        if (slot_changed) {
+            char prev_label[32];
+            if (previous_slot >= 0) {
+                snprintf(prev_label, sizeof(prev_label), "slot %d",
+                         previous_slot + 1);
+            } else {
+                strncpy(prev_label, "no slot", sizeof(prev_label) - 1);
+                prev_label[sizeof(prev_label) - 1] = '\0';
+            }
+            fprintf(stderr,
+                    "sync master: %s moved from %s to slot %d (generation %d)\n",
+                    id, prev_label, assigned_slot + 1, slot_generation);
+        }
+        if (send_generation > 0) {
+            fprintf(stderr,
+                    "sync master: sending slot %d generation %d commands to %s (prev_ack=%d, slot_generation=%d, reason=%s)\n",
+                    assigned_slot + 1, send_generation, id, previous_ack_generation,
+                    slot_generation, send_reason);
         }
         if (cfg.sync_slots[assigned_slot].name[0]) {
             strncpy(slot_label, cfg.sync_slots[assigned_slot].name,
